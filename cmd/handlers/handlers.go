4a252da36aa1e8f5a64df318d140a345a399f082
Victor Vayuta <eoll073@gmail.com>
2022-07-08 22:23:46 +0300

    1 st commit

---
+++ cmd/handlers/handlers.go
@@ -0,0 +1,48 @@
+package handlers
+
+import (
+	"errors"
+	"fmt"
+	"github.com/gorilla/mux"
+	"io"
+	"net/http"
+	"strconv"
+)
+
+func NameParam(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+
+	fmt.Fprintf(w, "Hello, "+params["PARAM"]+"!")
+}
+
+func BadParam(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusInternalServerError)
+}
+
+func BodyParam(w http.ResponseWriter, r *http.Request) {
+	body, err := io.ReadAll(r.Body)
+
+	if err != nil {
+		fmt.Fprintf(w, errors.New("Error reading http response").Error())
+		return
+	}
+
+	fmt.Fprintf(w, "I got message:\n"+string(body))
+}
+
+func HeadersParam(w http.ResponseWriter, r *http.Request) {
+	headerA := r.Header.Get("a")
+	headerB := r.Header.Get("b")
+
+	if headerA == "" || headerB == "" {
+		fmt.Fprintf(w, errors.New("Empty header!").Error())
+		return
+	}
+
+	valueA, errA := strconv.Atoi(headerA)
+	valueB, errB := strconv.Atoi(headerB)
+
+	if errA == nil && errB == nil {
+		w.Header().Set("a+b", strconv.Itoa(valueA+valueB))
+	}
+}
--- cmd/muxing/muxing.go
+++ cmd/muxing/muxing.go
@@ -7,19 +7,21 @@ import (
 	"os"
 	"strconv"
 
+	"github.com/GolangUnited/helloweb/cmd/handlers"
 	"github.com/gorilla/mux"
 )
 
-/**
-Please note Start functions is a placeholder for you to start your own solution.
-Feel free to drop gorilla.mux if you want and use any other solution available.
+func AddHandlers(router *mux.Router) {
+	router.HandleFunc("/name/{PARAM}", handlers.NameParam).Methods(http.MethodGet)
+	router.HandleFunc("/bad", handlers.BadParam).Methods(http.MethodGet)
+	router.HandleFunc("/data", handlers.BodyParam).Methods(http.MethodPost)
+	router.HandleFunc("/headers", handlers.HeadersParam).Methods(http.MethodPost)
+}
 
-main function reads host/port from env just for an example, flavor it following your taste
-*/
-
-// Start /** Starts the web server listener on given host and port.
 func Start(host string, port int) {
 	router := mux.NewRouter()
+
+	AddHandlers(router)
 
 	log.Println(fmt.Printf("Starting API server on %s:%d\n", host, port))
 	if err := http.ListenAndServe(fmt.Sprintf("%s:%d", host, port), router); err != nil {
